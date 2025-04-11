f9869b88826cce95c8e477d85913cac2ef05ceaf
Alex Ratchev <146139971+AlexR8878@users.noreply.github.com>
2025-04-10 20:30:46 -0400

    Started Millstone 2

--- NetworksTermProject/NetworksTermProject/MySocket.h
+++ NetworksTermProject/NetworksTermProject/MySocket.h
@@ -1,14 +1,14 @@
 #pragma once
-
-#pragma once
 #include <memory>
 #include <iostream>
 #include <fstream>
-#include <cstring>      
-#include <unistd.h>    
-#include <sys/socket.h>  
-#include <netinet/in.h>  
-#include <arpa/inet.h>
+#include <cstring>   
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")  
+using namespace std;
+
 
 const int DEFAULT_SIZE = 128;
 
@@ -23,9 +23,42 @@ enum ConnectionType {
 };
 
 
-
 class MySocket {
-	char* Buffer;
-	WelecomeSocket MySocket;
-	ConnectionSocket;
+    char* buffer;
+    int  WelcomeSocket;
+    int  ConnectionSocket;
+    int SvrAddr;
+    SocketType mySocket;
+    string IPAddr;
+    unsigned int port;
+    ConnectionType connectionType;
+    bool bTCPConnect;
+    unsigned int MaxSize;
+
+public:
+    MySocket(SocketType socketType, string IP, unsigned int portNum, ConnectionType connType, unsigned int maxSize = DEFAULT_SIZE) {
+        mySocket = socketType;
+        IPAddr = IP;
+        port = portNum;
+        connectionType = connType;
+        MaxSize = maxSize;
+        buffer = new char[maxSize];
+        memset(&SvrAddr, 0, sizeof(SvrAddr));
+        WelcomeSocket = -1;
+        ConnectionSocket = -1;
+        bTCPConnect = false;
+
+    }
+
+    ~MySocket() {
+        delete[] buffer;
+        if (WelcomeSocket != -1) {
+            closesocket(WelcomeSocket);
+        }
+        if (ConnectionSocket != -1) {
+            closesocket(ConnectionSocket);
+        }
+        WSACleanup();
+    }
 };
+
